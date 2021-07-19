4b8c3d5dc1b4513da65f66c3cd717ff16396547e
Cesar Oliva <you@example.com>
2021-07-19 14:26:30 -0500

    Terminando

--- JS/sticky.js
+++ JS/sticky.js
@@ -1,17 +1,3 @@
-// $(document).ready(function(){
-//     var windowWidth = $(window).width();
-//     if(windowWidth < 852){
-//         window.addEventListener("scroll", function(){
-//             const header = this.document.querySelector('header');
-//             header.classList.toggle('sticky', window.scrollY >= 0);
-//         })
-        
-//         document.querySelector('.toggle').onclick = function(){
-//             this.classList.toggle('active');
-//         }
-//     }
-// })
-
 $(document).ready(function(){
     var windowWidth = $(window).width();
     if(windowWidth < 852){
