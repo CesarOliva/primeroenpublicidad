a634e5eac5299d4cd7b3798f7a1e82609d952a78
Cesar Oliva <you@example.com>
2021-07-21 18:37:52 -0500

    Terminando javascript

--- JS/sticky.js
+++ JS/sticky.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    
+    // sticky
+    
     var windowWidth = $(window).width();
     if(windowWidth < 852){
         window.addEventListener("scroll", function(){
@@ -12,4 +15,28 @@ $(document).ready(function(){
             navigation.classList.toggle('active')
         }
     }
+
+    //imagenes modal
+
+    const overlay = document.getElementById('overlay');
+    document.querySelectorAll('.trabajos .row .contenedor .imagen-contenedor img').forEach((elemento) => {
+    
+    elemento.addEventListener('click', () => {
+            const ruta = elemento.getAttribute('src');
+    
+            overlay.classList.add('active');
+            document.querySelector('#overlay img').src = ruta
+        })
+    })
+
+    //event listener cerrar
+    document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+        overlay.classList.remove('active')
+    })
+
+    //eventlistener overlay
+    overlay.addEventListener('click', (evento) => {
+        // overlay.classList.remove('active');
+        evento.target.id === 'overlay' ? overlay.classList.remove('active') : '';
+    })
 })
