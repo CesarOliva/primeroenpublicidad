1fc703bcbb16148b4b9ac1abe6ad38dbb6bd541b
Cesar Oliva <you@example.com>
2021-07-22 13:33:14 -0500

    Agregando menu

--- JS/sticky.js
+++ JS/sticky.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
     
     // sticky
-    var comprobar = 'si' 
     var windowWidth = $(window).width();
     if(windowWidth < 852){
-        window.addEventListener("scroll", function(){
-            const header = this.document.querySelector('header');
-            header.classList.toggle('sticky', window.scrollY >=0);
-        })
+        // window.addEventListener("scroll", function(){
+        //     const header = this.document.querySelector('header');
+        //     header.classList.toggle('sticky', window.scrollY >=0);
+        // })
+
+        var Header = document.querySelector('header');
+        Header.className = 'sticky'
         
         const navigation = document.querySelector('nav')
         document.querySelector('.toggle').onclick = function(){
